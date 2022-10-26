7be1c7f63beb1d4c5edba511026754d702aec2b3
Paul Thomas <paulthomas2107@gmail.com>
2022-10-26 18:25:08 +0100

    Added more....

--- postcss.config.js
+++ postcss.config.js
@@ -1,5 +1,11 @@
 module.exports = {
     plugins: [
-        require('autoprefixer')
+        require('autoprefixer'),
+        require('postcss-preset-env')({
+            stage: 1,
+        }),
+        require('precss'),
+        require('postcss-import'),
+
     ]
 }
